ccdf9e76a5b5a1b27637ed3d3b6b6259b12d87a0
victo <victo@PC-VICTOR>
2018-12-18 12:59:57 +0100

    Es nuestro primer commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php
+
+echo'<head>
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+<title>ASIR Paloma</title>
+</head>
+<body>
+<div class="example">
+    <ul id="nav">
+        <li><a href="#">Home</a></li>
+        <li><a href="RealizarCopia.php">Realizar Copia Seguridad</a></li>
+        <li><a href="RestaurarCopia.php">Restaurar Copia Seguiridad</a></li>
+        <li><a href="#">Salir</a></li>
+    </ul>
+</div>
+
+</body>';
+
