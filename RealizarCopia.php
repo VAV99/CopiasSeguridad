ba602dcb082883702ae10473302ab168853eb403
victo <victo@PC-VICTOR>
2018-12-18 13:02:16 +0100

    Es nuestro tercer commit

--- RealizarCopia.php
+++ RealizarCopia.php
@@ -1,6 +1,6 @@
 <?php
 include 'header.php';
-
+echo'<h2>Este formulario es para hacer copias de seguridad</h2>';
 echo'<from action = "RealizarCopia.php>" method = "POST">
 Solo una BD
 <input type="radio" name="rbcopia" value="0" checked="checked"/> 
